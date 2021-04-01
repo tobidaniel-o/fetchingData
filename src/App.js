5e3af91fde6a8a7c4fe9d82bbd9e369eb764dbea
Tobi <tobi.daniel2018@gmail.com>
2021-04-01 12:47:19 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+// https://swapi.co/
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      character: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://swapi.dev/api/people/1")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ character: data });
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <div>{this.state.character.name}</div>
+      </>
+    );
+  }
 }
 
 export default App;
